db6176cb9111d5df6b325d5d74b4385633551761
Julián Martín <julian.martin@osoco.es>
2013-07-02 17:56:05 +0200

    minnor changes

--- SimplexMailGrailsPlugin.groovy
+++ SimplexMailGrailsPlugin.groovy
@@ -4,7 +4,7 @@ class SimplexMailGrailsPlugin {
     private static final log = LogFactory.getLog("es.osoco.simplexmail")
     
     // the plugin version
-    def version = "0.2"
+    def version = "0.3"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.0 > *"
     // the other plugins this plugin depends on
